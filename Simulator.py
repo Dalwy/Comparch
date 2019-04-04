7fd8e10d5cc15b2f37e735631db4b2b18c73514a
Sebastian Santana <37159083+crossflag@users.noreply.github.com>
2019-04-03 21:46:25 -0500

    Update Simulator.py
    added the main printing

--- Simulator.py
+++ Simulator.py
@@ -9,7 +9,9 @@ class Simulator:
 
     def run(self):
         global outputFile
+        global Memory
 
+        Memory = 96
         for i in range(len(sys.argv)):
             if (sys.argv[i] == '-i' and i < (len(sys.argv) - 1)):
                 inputFile = sys.argv[i + i]
@@ -21,27 +23,65 @@ class Simulator:
         i = 0
         while disassembler2.opcodeStr[i] != 'BREAK':
             if disassembler2.opcodeStr[i] == "ADDI":
-                outputFile.write("====================" + "\n" + "cycle" + str(i+1) + "\t" + str(disassembler2.Memory) +
-                                 "\t" + "ADDI" + "\n")
-            elif disassembler2.opcodeStr[i] == "B":
-                outputFile.write("====================" + "\n" + "cycle" + str(i + 1) + "\t"
-                                 + "B" + "\n")
+                outputFile.write("====================" + "\n" + "cycle " + str(i+1) + "\t" + str(Memory)
+                                 + "\t" + "ADDI" + "\n")
+            elif disassembler2.opcodeStr[i] == "B ":
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" + "B" + "\n")
             elif disassembler2.opcodeStr[i] == "ADD ":
-                outputFile.write("====================" + "\n" + "cycle" + str(i + 1) + "\t" + "\n")
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" + "ADD" + "\n")
             elif disassembler2.opcodeStr[i] == "SUB ":
-                outputFile.write("====================" + "\n" + "cycle" + str(i + 1) + "\t" + "\n")
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" + "SUB" + "\n")
             elif disassembler2.opcodeStr[i] == "SUBI":
-                outputFile.write("====================" + "\n" + "cycle" + str(i + 1) + "\t" + "\n")
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" + "SUBI" + "\n")
             elif disassembler2.opcodeStr[i] == "AND ":
-                outputFile.write("====================" + "\n" + "cycle" + str(i + 1) + "\t" + "\n")
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" + "AND" + "\n")
             elif disassembler2.opcodeStr[i] == "ORR ":
-                outputFile.write("====================" + "\n" + "cycle" + str(i + 1) + "\t" + "\n")
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" + "ORR" + "\n")
             elif disassembler2.opcodeStr[i] == "EOR ":
-                outputFile.write("====================" + "\n" + "cycle" + str(i + 1) + "\t" + "\n")
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" + "EOR" + "\n")
             elif disassembler2.opcodeStr[i] == "MOVZ":
-                outputFile.write("====================" + "\n" + "cycle" + str(i + 1) + "\t" + "\n")
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" + "MOVZ" + "\n")
+            elif disassembler2.opcodeStr[i] == "MOVK":
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" + "MOVK" + "\n")
+            elif disassembler2.opcodeStr[i] == "STUR":
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" + "STUR" + "\n")
+            elif disassembler2.opcodeStr[i] == "LDUR":
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" + "LDUR" + "\n")
+            elif disassembler2.opcodeStr[i] == "CBZ ":
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" "CBZ" + "\n")
+            elif disassembler2.opcodeStr[i] == "CBNZ":
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" + "CBNZ" + "\n")
+            elif disassembler2.opcodeStr[i] == "ASR":
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" + "ASR" + "\n")
+            elif disassembler2.opcodeStr[i] == "LSL ":
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" + "LSL" + "\n")
+            elif disassembler2.opcodeStr[i] == "LSR ":
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" + "LSR" + "\n")
+            elif disassembler2.opcodeStr[i] == "NOP":
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" + "NOP" + "\n")
+            elif disassembler2.opcodeStr[i] == "BREAK":
+                outputFile.write("====================" + "\n" + "cycle " + str(i + 1) + "\t" + str(Memory)
+                                 + "\t" + "BREAK" + "\n")
             print disassembler2.opcodeStr[i]
             i += 1
+            Memory += 4
 if __name__ == '__main__':
     test = Simulator()
     test.run()
